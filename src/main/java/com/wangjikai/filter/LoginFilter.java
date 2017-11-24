ea59bf0ba9a0c47c258fe200f6c0a7d83d70c7ac
zhujianming <XX  cecgw.cn;git config --global user.email cecgw.cngit config --global user.email XX>
2017-11-24 12:20:26 +0800

    除了登录页不拦截外，其余页面均被拦截在外

---
+++ src/main/java/com/wangjikai/filter/LoginFilter.java
@@ -0,0 +1,35 @@
+package com.wangjikai.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 22717 on 2017/11/24.
+ * 登陆校验
+ */
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("初始化");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String path = request.getRequestURI();
+        //除了登录页不拦截（整个网站只能显示登录页）外，其余页面均被拦截在外
+        if ("/views/login.jsp".equals(path) || path.contains(".css")) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            response.sendRedirect("/views/login.jsp");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("销毁");
+    }
+}
